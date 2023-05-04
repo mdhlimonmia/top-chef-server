835fd05e8328ddaca08ae4a599173f34c8c580da
Mdhlimon <mdhlimonmia@gmail.com>
2023-05-04 18:26:10 +0600

    add json file and send data to cient site

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
 })
 
 app.listen(port, () => {
